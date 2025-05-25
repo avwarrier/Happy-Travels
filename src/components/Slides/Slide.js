79e351d1175c70a613d195956c8f2cc6c82f3273
avwarrier <avwarrier@ucdavis.edu>
2025-05-24 18:01:01 -0700

    basic template

--- src/components/Slides/Slide.js
+++ src/components/Slides/Slide.js
@@ -7,7 +7,7 @@ const Slide = ({ children, prev, next, welcome, end, ...rest }) => {
         <div className='flex flex-col items-center h-screen justify-center gap-[20px]'>
             {
                 !welcome ?
-                    <button onClick={prev} className='bg-[#191919] rounded-md shadow-md text-white w-[100px] h-[45px]'>
+                    <button onClick={prev} className='bg-[#191919] rounded-md shadow-md text-white w-[100px] h-[45px] cursor-pointer'>
                         <KeyboardArrowUpIcon sx={{ fontSize: 30 }} />
                     </button>
                 :
@@ -20,7 +20,7 @@ const Slide = ({ children, prev, next, welcome, end, ...rest }) => {
 
             {
                 !end ?
-                    <button onClick={next} className='bg-[#191919] rounded-md shadow-md text-white w-[100px] h-[45px]'>
+                    <button onClick={next} className='bg-[#191919] rounded-md shadow-md text-white w-[100px] h-[45px] cursor-pointer'>
                         <KeyboardArrowDownIcon sx={{ fontSize: 30 }} />
                     </button>
                 :
