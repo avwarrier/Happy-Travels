cc445ed9632e39bda6dc1337682d2e46d75f935c
avwarrier <avwarrier@ucdavis.edu>
2025-05-29 20:35:41 -0700

    algorithm done

---
+++ src/util/structures.js
@@ -0,0 +1,166 @@
+export const cityAvgData = {
+    weekdays: {
+        Amsterdam: {
+            price: 545.02,
+            cleanliness: 9.46,
+            satisfaction: 94.36,
+            distance: 2.84,
+            capacity: 2.79,
+            superhost_pct: 29.28
+        },
+        Athens: {
+            price: 155.87,
+            cleanliness: 9.64,
+            satisfaction: 95.10,
+            distance: 1.78,
+            capacity: 3.71,
+            superhost_pct: 43.08
+        },
+        Barcelona: {
+            price: 288.39,
+            cleanliness: 9.29,
+            satisfaction: 90.93,
+            distance: 2.12,
+            capacity: 2.76,
+            superhost_pct: 18.07
+        },
+        Berlin: {
+            price: 240.22,
+            cleanliness: 9.48,
+            satisfaction: 94.30,
+            distance: 5.26,
+            capacity: 2.80,
+            superhost_pct: 26.64
+        },
+        Budapest: {
+            price: 168.43,
+            cleanliness: 9.47,
+            satisfaction: 9.47,
+            distance: 1.90,
+            capacity: 3.58,
+            superhost_pct: 36.89
+        },
+        Lisbon: {
+            price: 236.35,
+            cleanliness: 9.36,
+            satisfaction: 91.05,
+            distance: 1.96,
+            capacity: 3.37,
+            superhost_pct: 21.35
+        },
+        London: {
+            price: 360.23,
+            cleanliness: 9.15,
+            satisfaction: 90.32,
+            distance: 5.33,
+            capacity: 2.83,
+            superhost_pct: 14.69
+        },
+        Paris: {
+            price: 398.79,
+            cleanliness: 9.25,
+            satisfaction: 91.85,
+            distance: 3.01,
+            capacity: 2.95,
+            superhost_pct: 13.71
+        },
+        Rome: {
+            price: 201.62,
+            cleanliness: 9.52,
+            satisfaction: 93.20,
+            distance: 3.04,
+            capacity: 3.35,
+            superhost_pct: 33.64
+        },
+        Vienna: {
+            price: 240.38,
+            cleanliness: 9.47,
+            satisfaction: 93.80,
+            distance: 3.14,
+            capacity: 3.33,
+            superhost_pct: 27.52
+        }
+    },
+    weekends: {
+        Amsterdam: {
+            price: 604.83,
+            cleanliness: 9.47,
+            satisfaction: 94.69,
+            distance: 2.81,
+            capacity: 2.77,
+            superhost_pct: 27.43
+        },
+        Athens: {
+            price: 147.58,
+            cleanliness: 9.63,
+            satisfaction: 94.91,
+            distance: 1.82,
+            capacity: 3.69,
+            superhost_pct: 42.63
+        },
+        Barcelona: {
+            price: 300.28,
+            cleanliness: 9.30,
+            satisfaction: 91.33,
+            distance: 2.12,
+            capacity: 2.45,
+            superhost_pct: 18.23
+        },
+        Berlin: {
+            price: 249.25,
+            cleanliness: 9.45,
+            satisfaction: 94.35,
+            distance: 5.25,
+            capacity: 2.75,
+            superhost_pct: 24.75
+        },
+        Budapest: {
+            price: 185.12,
+            cleanliness: 9.49,
+            satisfaction: 94.65,
+            distance: 1.84,
+            capacity: 3.50,
+            superhost_pct: 38.96
+        },
+        Lisbon: {
+            price: 240.04,
+            cleanliness: 9.38,
+            satisfaction: 91.14,
+            distance: 1.97,
+            capacity: 3.32,
+            superhost_pct: 21.44
+        },
+        London: {
+            price: 364.39,
+            cleanliness: 9.19,
+            satisfaction: 90.92,
+            distance: 5.33,
+            capacity: 2.86,
+            superhost_pct: 16.64
+        },
+        Paris: {
+            price: 387.03,
+            cleanliness: 9.27,
+            satisfaction: 92.20,
+            distance: 2.98,
+            capacity: 2.96,
+            superhost_pct: 14.39
+        },
+        Rome: {
+            price: 209.13,
+            cleanliness: 9.51,
+            satisfaction: 93.05,
+            distance: 3.01,
+            capacity: 3.36,
+            superhost_pct: 31.71
+        },
+        Vienna: {
+            price: 242.74,
+            cleanliness: 9.47,
+            satisfaction: 93.67,
+            distance: 3.14,
+            capacity: 3.37,
+            superhost_pct: 27.52
+        }
+    }
+}
