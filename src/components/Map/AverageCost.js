2b0a8b6a82dd225fc43611fe01318995df654b68
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-06-04 12:25:07 -0700

    update cost to usd

--- src/components/Map/AverageCost.js
+++ src/components/Map/AverageCost.js
@@ -2,13 +2,13 @@ export const AverageCost = ({avgCost, weekdayAvgCost, weekendAvgCost}) => {
     return (
     <div className="p-2 border border-gray-200 rounded-lg shadow-sm bg-gray-50 h-44 w-36 flex flex-col items-center">
         <h2 className="font-semibold">Average Price</h2>
-        <h2 className="text-green-600 font-bold text-2xl pb-4">€{parseFloat(avgCost).toFixed(2)}</h2>
+        <h2 className="text-green-600 font-bold text-2xl pb-4">${parseFloat(avgCost).toFixed(2)}</h2>
         
         <h2 className="font-semibold">Weekday Cost</h2>
-        <h2 className="text-green-600 font-semibold">€{parseFloat(weekdayAvgCost).toFixed(2)}</h2>
+        <h2 className="text-green-600 font-semibold">${parseFloat(weekdayAvgCost).toFixed(2)}</h2>
 
         <h2 className="font-semibold">Weekend Cost</h2>
-        <h2 className="text-green-600 font-semibold">€{parseFloat(weekendAvgCost).toFixed(2)}</h2>
+        <h2 className="text-green-600 font-semibold">${parseFloat(weekendAvgCost).toFixed(2)}</h2>
     </div>
   );
 }
